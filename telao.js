b8ffe9eea7e7a7225cb8869631841090616b9207
TUNAPbr <dev.tunapbr@gmail.com>
2025-11-27 14:44:36 -0300

    Update telao.js

--- telao.js
+++ telao.js
@@ -7,8 +7,8 @@
 // CONFIGURAÇÃO SUPABASE
 // ============================================
 
-const SUPABASE_URL = 'SUA_URL_AQUI';
-const SUPABASE_ANON_KEY = 'SUA_CHAVE_AQUI';
+const SUPABASE_URL = 'https://qsztainariaiznbblrap.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzenRhaW5hcmlhaXpuYmJscmFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MDQ1MDUsImV4cCI6MjA3OTQ4MDUwNX0.y9RYzXjnPaNo6Mbi_76W_MFBXHjpXmj-rvkvLYkIP4k';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
